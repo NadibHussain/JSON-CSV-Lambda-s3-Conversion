07bb88f820299cc18b500794aa3799b4f1d5a4f8
NadibHussain <nadib2015@gmail.com>
2023-09-03 18:43:24 -0400

    added quicksight manifest

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 
-module "lemdaModule" {
-  source = "./modules/lemda"
+module "lambdaModule" {
+  source = "./modules/lambda"
 }
 
 module "s3Module" {
   source = "./modules/s3"
-  lemda_role_name = module.lemdaModule.lemda_role_output
-  lemda_role_arn = module.lemdaModule.lambda_role_arn_output
+  lemda_role_name = module.lambdaModule.lemda_role_output
+  lemda_role_arn = module.lambdaModule.lambda_role_arn_output
 }
 
 
@@ -14,5 +14,5 @@ module "triggerModule" {
   source = "./modules/trigger"
   s3_bucket_id = module.s3Module.aws_s3_bucket_id
   s3_bucket_arn = module.s3Module.aws_s3_bucket_arn
-  lemda_function_arn = module.lemdaModule.teraform_aws_function_arn_output
+  lemda_function_arn = module.lambdaModule.teraform_aws_function_arn_output
 }
